51b5dbe7c0cc93dfbe7de2fedc71c81769045323
Sakibul Islam <sakibulislam25800@gmail.com>
2024-11-15 08:57:29 +0000

    Remove setSearchParams

--- plugins/hubspot/src/hooks/useSearchParams.ts
+++ plugins/hubspot/src/hooks/useSearchParams.ts
@@ -1,9 +1,8 @@
-import { useSearch, useLocation } from "wouter"
-import { useMemo, useCallback } from "react"
+import { useSearch } from "wouter"
+import { useMemo } from "react"
 
 export const useSearchParams = <T extends Record<string, string>>() => {
     const searchString = useSearch()
-    const [, setLocation] = useLocation()
 
     // Parse current search params
     const params = useMemo(() => {
@@ -11,27 +10,5 @@ export const useSearchParams = <T extends Record<string, string>>() => {
         return Object.fromEntries(searchParams.entries()) as T
     }, [searchString])
 
-    const setSearchParams = useCallback(
-        (newParams: Partial<T>) => {
-            const currentParams = new URLSearchParams(searchString)
-
-            // Update or add new params
-            Object.entries(newParams).forEach(([key, value]) => {
-                if (value === null || value === undefined) {
-                    currentParams.delete(key)
-                } else {
-                    currentParams.set(key, value)
-                }
-            })
-
-            const pathname = location.pathname
-            const newSearch = currentParams.toString()
-            const newUrl = `${pathname}${newSearch ? `?${newSearch}` : ""}`
-
-            setLocation(newUrl)
-        },
-        [searchString, setLocation]
-    )
-
-    return [params, setSearchParams] as const
+    return [params] as const
 }
